96cf6954ca932a384afc774fe7b3fa6061007682
Daddy <sergoffan@mail.ru>
2022-11-26 23:15:04 +0300

    добавил решение

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество элементов массива: ");
+int amount = Convert.ToInt32(Console.ReadLine());
+string[] arrList = new string[amount];
+string[] arrTotal = new string[amount];
+int length = 3;
+int count = 0;
